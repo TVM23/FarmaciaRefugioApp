5516f752f7e859b9b12e7a40a2f15039c757a0a7
TVM23 <tadeo23manzo@outlook.com>
2024-03-02 21:48:39 -0600

    UnidadTrabajo

--- FarmaciaRefugio/Program.cs
+++ FarmaciaRefugio/Program.cs
@@ -1,4 +1,6 @@
 using FarmaciaRefugio.AccesoDatos.Data;
+using FarmaciaRefugio.AccesoDatos.Repositorio.IRepositorio;
+using FarmaciaRefugio.AccesoDatos.Repositorio;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUnidadTrabajo, UnidadTrabajo>();
 
 var app = builder.Build();
 
